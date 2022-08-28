c1c0968b6052b22504adf854b3315897793d1705
applyml2020 <112341608+applyml2020@users.noreply.github.com>
2022-08-28 11:51:28 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='toolbox',
+    version='0.0.1',
+    description='Data science tools.',
+    url='http://github.com/applyml2020/toolbox',
+    author='ApplyML',
+    author_email='applyml2020@gmail.com',
+    license='MIT',
+    packages=['toolbox'],
+    zip_safe=False,
+)
